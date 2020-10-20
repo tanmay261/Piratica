c2fb4384ddf393517673bc29d9b4c72fe036d9ab
Tanmay Agrawal <tanmaytanu.10@gmail.com>
2020-10-20 22:00:57 +0530

    🙌homeScreen Updated

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,79 +10,83 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Container(
-        width: double.infinity,
-        color: Colors.white,
-        padding: EdgeInsets.all(40.0),
-        child: Stack(children: [
-          Positioned(
-              child: SvgPicture.asset(
-            'assets/social.svg',
-            height: size.height * 0.45,
-          )),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Welcome !',
-                style: TextStyle(
-                    fontSize: 30.0,
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold),
-              ),
-              SizedBox(
-                height: 5.0,
-              ),
-              Text(
-                'How would you like to Sign Up ?',
-                style: TextStyle(fontSize: 15.0, color: Colors.black),
-              ),
-              SizedBox(
-                height: 100.0,
-              ),
-              Container(
-                margin: EdgeInsets.only(top: 100.0),
-                // padding: EdgeInsets.only(top: 10.0),
-
-                height: 400.0,
-                width: double.infinity,
-                color: Colors.white,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    SignInContainer(Colors.blue, 'Continue with Email',
-                        Colors.white, 'assets/email.png'),
-                    SizedBox(height: 30.0),
-                    SignInContainer(Colors.black, 'Continue with Google',
-                        Colors.white, 'assets/google.png'),
-                    SizedBox(height: 30.0),
-                    SignInContainer(Colors.green, 'Continue with Apple',
-                        Colors.white, 'assets/apple.png'),
-                    SizedBox(height: 30.0),
-                    Text(
-                      'Already have an Account ?',
-                      style: TextStyle(fontSize: 15.0, color: Colors.black),
-                    ),
-                    GestureDetector(
-                      child: Text(
-                        'Sign In',
-                        style: TextStyle(
-                            fontSize: 18.0,
-                            color: Colors.black,
-                            fontWeight: FontWeight.bold,
-                            decoration: TextDecoration.underline),
-                      ),
-                      onTap: () {
-                        Navigator.pushNamed(context, 'Sign In');
-                      },
-                    )
-                  ],
+      body: SingleChildScrollView(
+              child: Container(
+          width: double.infinity,
+          color: Colors.white,
+          padding: EdgeInsets.all(40.0),
+          child: Stack(children: [
+            Positioned(
+                child: SvgPicture.asset(
+              'assets/social.svg',
+              height: size.height * 0.45,
+            )),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Welcome !',
+                  style: TextStyle(
+                      fontSize: 30.0,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold),
                 ),
-              )
-            ],
-          ),
-        ]),
+                SizedBox(
+                  height: 5.0,
+                ),
+                Text(
+                  'How would you like to Sign Up ?',
+                  style: TextStyle(fontSize: 15.0, color: Colors.black),
+                ),
+                SizedBox(
+                  height: 100.0,
+                ),
+                Container(
+                  
+                  color: Colors.white,
+                  margin: EdgeInsets.only(top: 100.0),
+                  // padding: EdgeInsets.only(top: 10.0),
+
+                  height: 400.0,
+                  width: double.infinity,
+
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SignInContainer(Colors.blue, 'Continue with Email',
+                          Colors.white, 'assets/email.png'),
+                      SizedBox(height: 30.0),
+                      SignInContainer(Colors.black, 'Continue with Google',
+                          Colors.white, 'assets/google.png'),
+                      SizedBox(height: 30.0),
+                      SignInContainer(Colors.green, 'Continue with Apple',
+                          Colors.white, 'assets/apple.png'),
+                      SizedBox(height: 30.0),
+                      Text(
+                        'Already have an Account ?',
+                        style: TextStyle(fontSize: 15.0, color: Colors.black),
+                      ),
+                      GestureDetector(
+                        child: Text(
+                          'Sign In',
+                          style: TextStyle(
+                              fontSize: 18.0,
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              decoration: TextDecoration.underline),
+                        ),
+                        onTap: () {
+                          Navigator.pushNamed(context, 'Sign In');
+                        },
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ]),
+        ),
       ),
     );
   }
