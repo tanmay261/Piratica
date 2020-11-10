368739de7122d92d49a81e56253442423a3f9566
Tanmay Agrawal <tanmaytanu.10@gmail.com>
2020-11-11 00:29:33 +0530

    added list of drawer menu options

--- lib/widgets/constants.dart
+++ lib/widgets/constants.dart
@@ -1,20 +1,24 @@
+
+
+import 'package:PiraticaApp/screens/leaderboard.dart';
+import 'package:PiraticaApp/screens/profile_page.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
 
 const kPrimaryColor = Color(0xFF112d4e);
 const kOtherColor = Color(0xFF3f72af);
 const kPrimaryLightColor = Color(0xFFf9f7f7);
 const drawerTextColor = Color(0xFFdbe2ef);
 List<BoxShadow> shadowList = [
-  BoxShadow(color: Colors.grey[300], blurRadius: 30, offset: Offset(0, 10))
+  BoxShadow(color: Colors.grey[400], blurRadius: 30, offset: Offset(0, 10))
 ];
 
 List<Map> drawerItems = [
-  {'icon': Icons.account_circle, 'title': 'Account Settings'},
-  {'icon': Icons.history, 'title': 'History'},
-  {'icon': Icons.notifications, 'title': 'Notification Settings'},
-  {'icon': Icons.list, 'title': 'Leaderboard'},
-  {'icon': Icons.access_time, 'title': 'Rewards'},
-  {'icon': Icons.palette, 'title': 'Goodies'},
+  {'icon': Icons.account_circle, 'title': 'Account Settings','onTap':ProfilePage.id},
+  {'icon': Icons.history, 'title': 'History','onTap':Leaderboard.id},
+  {'icon': Icons.notifications, 'title': 'Notification Settings','onTap':Leaderboard.id},
+  {'icon': Icons.list, 'title': 'Leaderboard','onTap':Leaderboard.id},
+  {'icon': Icons.access_time, 'title': 'Rewards','onTap':Leaderboard.id},
+  {'icon': Icons.palette, 'title': 'Goodies','onTap':Leaderboard.id},
 ];
 //Color(0xFFc4c0ff)
