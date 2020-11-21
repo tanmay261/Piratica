c5a0d05c5a0f3063c531f3b6e9f352b712dc619b
Tanmay Agrawal <tanmaytanu.10@gmail.com>
2020-11-21 15:38:34 +0530

    resolving tests commit

--- lib/models/story_model.dart
+++ lib/models/story_model.dart
@@ -1,14 +1,23 @@
 import 'package:meta/meta.dart';
 import 'models.dart';
 
+enum MediaType {
+  image,
+  video,
+}
+
 class Story {
   final User user;
   final String imageUrl;
   final bool isViewed;
+  final MediaType media;
+  final Duration duration;
 
   const Story({
     @required this.user,
     @required this.imageUrl,
     this.isViewed = false,
+    this.media,
+    this.duration,
   });
 }
