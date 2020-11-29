00e277b398949c74e1a96aafb6a280a35973d27e
Tanmay Agrawal <tanmaytanu.10@gmail.com>
2020-11-29 13:50:14 +0530

    👍drawer screen added to chats

---
+++ lib/screens/chatScreen/chatHomeScreen.dart
@@ -0,0 +1,21 @@
+import 'package:PiraticaApp/screens/chatScreen/chatscreen.dart';
+import 'package:PiraticaApp/screens/drawer_screen.dart';
+import 'package:PiraticaApp/screens/eventsScreen/events.dart';
+import 'package:flutter/material.dart';
+
+class ChatHomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<ChatHomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(children: [
+        DrawerScreen(),
+        ChatScreen(),
+      ]),
+    );
+  }
+}
