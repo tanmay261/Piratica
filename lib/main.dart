aeca4ac193a5f479e3ceba5a203bcf2bf0d06724
Tanmay Agrawal <tanmaytanu.10@gmail.com>
2020-11-29 13:46:39 +0530

    👌 added device preview

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,17 @@
+import 'package:PiraticaApp/screens/event_history.dart';
 import 'package:PiraticaApp/screens/leaderboard.dart';
 
 import 'package:PiraticaApp/screens/nav_screens.dart';
 import 'package:PiraticaApp/screens/profile_page.dart';
 import 'package:PiraticaApp/screens/signIn_page.dart';
 import 'package:PiraticaApp/screens/home_screen.dart';
+import 'package:PiraticaApp/screens/transaction_history.dart';
 import 'package:PiraticaApp/widgets/constants.dart';
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(DevicePreview(builder: (context) => MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -16,6 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      builder: DevicePreview.appBuilder,
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
@@ -31,6 +35,9 @@ class MyApp extends StatelessWidget {
         ProfilePage.id: (context) => ProfilePage(),
         Leaderboard.id: (context) => Leaderboard(),
         NavScreen.id: (context) => NavScreen(),
+        EventHistory.id:(context)=>EventHistory(),
+        TransactionHistory.id:(context)=>TransactionHistory(),
+
       },
       initialRoute: NavScreen.id,
     );
